e72580a7bfd65cfb81d3367237dad40a181f89ff
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-03 16:24:02 +0200

    Erstellen der Klasse UhrStart

---
+++ AnalogUhr/UhrStart.java
@@ -0,0 +1,11 @@
+package goebel;
+/**
+ * Startklasse fuer AnalogUhr.
+ * @author Melanie Goebel
+ * @version 2014-10-03
+ */
+public class UhrStart{
+	public static void main (String[] args){
+		
+	}
+}
