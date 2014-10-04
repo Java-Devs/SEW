626c7f854a4fd0e64cb98793c69d2a9c9cd0113b
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-04 09:55:33 +0200

    Stunden- und Minutenstriche hinzugefuegt

--- AnalogUhr/UhrDisplay.java
+++ AnalogUhr/UhrDisplay.java
@@ -11,28 +11,47 @@ import javax.swing.*;
  */
 public class UhrDisplay extends JPanel{
 	private int mittelpunkt;
+	private int radius;
 	
 	@Override
 	public void paint(Graphics g) {
-		int radius = 0;
 		g.setColor(Color.GRAY);
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.fillRect(0, 0, this.getWidth(), this.getHeight()); // Hintergrund in grau
 		g.setColor(new Color(152,223,243));
-		if(this.getWidth()>=this.getHeight()){
+		if(this.getWidth()>=this.getHeight()){ // Damit sich die Uhr anpasst und zu kein Oval wird
 			radius = this.getHeight();
+			mittelpunkt = radius/2;
 		}else{
 			radius = this.getWidth();
+			mittelpunkt = radius/2;
 		}
-		mittelpunkt = radius/2;
-		g.fillOval(0, 0, radius, radius);
+		g.fillOval(0, 0, radius, radius); // Die Uhr
 		g.setColor(Color.BLACK);
-		g.fillOval(radius/2-radius/80, radius/2-radius/80, radius/40, radius/40);
+		g.fillOval(radius/2-radius/80, radius/2-radius/80, radius/40, radius/40); // Den Mittelpunkt kennzeichnen
+		for (int i = 0; i < 12; i++) { // Die Striche fuer die Stunden
+			g.drawLine(gradToX(i*360/12,radius/2),gradToY(i*360/12,radius/2),gradToX(i*360/12,(int)(radius/2.5)),gradToY(i*360/12,(int)(radius/2.5)));
+		}
+		for (int i = 0; i < 60; i++) { // Die Striche fuer die Minuten
+			g.drawLine(gradToX(i*360/60,radius/2),gradToY(i*360/60,radius/2),gradToX(i*360/60,(int)(radius/2.1)),gradToY(i*360/60,(int)(radius/2.1)));
+		}
 	}
+	/**
+	 * Rechnet von einer Gradanzahl und Radius eines Kreis den x-Wert aus.
+	 * @param grad die Gradanzahl
+	 * @param r den Radius
+	 * @return den x-Wert
+	 */
 	public int gradToX(double grad, int r) { 
-		return (int)(mittelpunkt + r * Math.cos(Math.toRadians(grad)-Math.PI/2)+0.5); 
-	} 
+		return (int)((radius/2) + r * Math.cos(Math.toRadians(grad)-Math.PI/2)+0.5); 
+	}
+	/**
+	 * Rechnet von einer Gradanzahl und Radius eines Kreis den y-Wert aus.
+	 * @param grad die Gradanzahl
+	 * @param r den Radius
+	 * @return den y-Wert
+	 */
 	public int gradToY(double grad, int r) {
-		return (int)(mittelpunkt + r * Math.sin(Math.toRadians(grad)-Math.PI/2)+0.5); 
+		return (int)((radius/2) + r * Math.sin(Math.toRadians(grad)-Math.PI/2)+0.5); 
 	}
 
 }
