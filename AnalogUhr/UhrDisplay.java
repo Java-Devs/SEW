84dacb36e39f08d2eadc71bad6d9553597a4da39
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-04 09:25:08 +0200

    Variable radius hinzugefuegt

--- AnalogUhr/UhrDisplay.java
+++ AnalogUhr/UhrDisplay.java
@@ -14,24 +14,19 @@ public class UhrDisplay extends JPanel{
 	
 	@Override
 	public void paint(Graphics g) {
-		int laenge = this.getHeight();
-		int breite = this.getWidth();
+		int radius = 0;
 		g.setColor(Color.GRAY);
-		g.fillRect(0, 0, breite, laenge);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		g.setColor(new Color(152,223,243));
-		if(breite>=laenge){
-			mittelpunkt = laenge/2;
-			g.fillOval(0, 0, laenge, laenge);
-			g.setColor(Color.BLACK);
-			g.fillOval(laenge/2-laenge/80, laenge/2-laenge/80, laenge/40, laenge/40);
-
+		if(this.getWidth()>=this.getHeight()){
+			radius = this.getHeight();
 		}else{
-			mittelpunkt = breite/2;
-			g.fillOval(0, 0, breite, breite);
-			g.setColor(Color.BLACK);
-			g.fillOval(breite/2-breite/80, breite/2-breite/80, breite/40, breite/40);
-
+			radius = this.getWidth();
 		}
+		mittelpunkt = radius/2;
+		g.fillOval(0, 0, radius, radius);
+		g.setColor(Color.BLACK);
+		g.fillOval(radius/2-radius/80, radius/2-radius/80, radius/40, radius/40);
 	}
 	public int gradToX(double grad, int r) { 
 		return (int)(mittelpunkt + r * Math.cos(Math.toRadians(grad)-Math.PI/2)+0.5); 
