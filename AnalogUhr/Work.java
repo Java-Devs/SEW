85c8c58412d25a728e8230d0bdcd759d3c599cab
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-08 10:36:40 +0200

    Hinzufuegen der Berechnung von der Mondphase

--- AnalogUhr/Work.java
+++ AnalogUhr/Work.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
  */
 public class Work{
 	private Calendar cal = Calendar.getInstance();
-	
+
 	public String getWochentag(){
 		int a = cal.get(Calendar.DAY_OF_WEEK);
 		switch(a){
@@ -32,7 +32,8 @@ public class Work{
 	}
 	public String[] getDatum(){
 		cal = Calendar.getInstance();
-		return new String[]{""+cal.get(Calendar.YEAR),""+cal.get(Calendar.MONTH),""+cal.get(Calendar.DAY_OF_MONTH),getWochentag()};
+		int month = cal.get(Calendar.MONTH)+1;
+		return new String[]{""+cal.get(Calendar.YEAR),""+month,""+cal.get(Calendar.DAY_OF_MONTH),getWochentag()};
 	}
 	public int[] getZeit(){
 		cal = Calendar.getInstance();
@@ -57,5 +58,44 @@ public class Work{
 	 */
 	public int gradToY(double grad,int durchmesser, int r) {
 		return (int)((durchmesser/2) + r * Math.sin(Math.toRadians(grad)-Math.PI/2)+0.5); 
+	}
+	public int getVollmond(){
+		cal = Calendar.getInstance();
+		int dayofyear =  cal.get(Calendar.DAY_OF_YEAR);
+		if(cal.get(Calendar.YEAR)==2014){
+			int vollmond = 8;
+
+
+		}
+		return 0;
+	}
+	/**
+	 * @author http://www.voidware.com/moon_phase.htm
+	 * @param y das Jahr
+	 * @param m das Monat
+	 * @param d den Tag
+	 * @return die Phase des Mondes (zwischen 0-7, von zunehmend zu abnehmend)
+	 */
+	public int moonPhase(int y, int m, int d) {
+
+		double c;
+		double e;
+		double jd;
+		int b;
+
+		if (m < 3) {
+			y--;
+			m += 12;
+		}
+		++m;
+		c = 365.25 * y;
+		e = 30.6 * m;
+		jd = c + e + d - 694039.09; /* jd is total days elapsed */
+		jd /= 29.53; /* divide by the moon cycle (29.53 days) */
+		b = (int)jd; /* int(jd) -> b, take integer part of jd */
+		jd -= b; /* subtract integer part to leave fractional part of original jd */
+		b = (int)(jd * 8 + 0.5); /* scale fraction from 0-8 and round by adding 0.5 */
+		b = b & 7; /* 0 and 8 are the same so turn 8 into 0 */
+		return b;
 	}
 }
