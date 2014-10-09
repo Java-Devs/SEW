97aabf8da12ba6d0817b94ec0dacb8659f964d26
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-09 10:20:43 +0200

    Wochentage in einem Array, anstatt mit einen Switch suchen

--- AnalogUhr/Work.java
+++ AnalogUhr/Work.java
@@ -12,23 +12,8 @@ public class Work{
 
 	public String getWochentag(){
 		int a = cal.get(Calendar.DAY_OF_WEEK);
-		switch(a){
-		case 1:
-			return "So";
-		case 2:
-			return "Mo";
-		case 3:
-			return "Di";
-		case 4:
-			return "Mi";
-		case 5:
-			return "Do";
-		case 6:
-			return "Fr";
-		case 7:
-			return "Sa";
-		}
-		return "Error";
+		String[] days = {"So","Mo","Di","Mi","Do","Fr","Sa"};
+		return days[a-1];
 	}
 	public String[] getDatum(){
 		cal = Calendar.getInstance();
