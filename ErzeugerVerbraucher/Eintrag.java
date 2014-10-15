16f8d4826d7d6c81387a7314a65bda71e86f0f0b
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-15 15:38:41 +0200

    Methodenkommentare hinzugefuegt

--- ErzeugerVerbraucher/Eintrag.java
+++ ErzeugerVerbraucher/Eintrag.java
@@ -9,13 +9,26 @@ public class Eintrag{
 	private String name;
 	private int anzahl;
 	
+	/**
+	 * Konstruktor mit Werten fuer den Eintrag
+	 * @param name den Name des Eintrages
+	 * @param anzahl die Anzahl die zu speichern ist vom Eintrag
+	 */
 	public Eintrag(String name, int anzahl){
 		this.name = name;
 		this.anzahl = anzahl;
 	}
+	/**
+	 * Getter-Methode fuer den Namen
+	 * @return den Namen als String
+	 */
 	public String getName(){
 		return name;
 	}
+	/**
+	 * Getter-Methode fuer die Anzahl
+	 * @return die Anzahl in int
+	 */
 	public int getAnzahl(){
 		return anzahl;
 	}
