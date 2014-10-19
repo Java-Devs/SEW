647677b392a2f201705939efdebe8d7d65a03141
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-20 00:34:38 +0200

    Konstruktor erstellt und Ausgabe des Statuses vom Lager

--- ErzeugerVerbraucher/Status.java
+++ ErzeugerVerbraucher/Status.java
@@ -1,9 +1,27 @@
 package goebel;
 public class Status implements Runnable{
+	private Lager l;
+	private int maxtaktzeit;
+	
+	public Status(Lager l, int maxtaktzeit){
+		this.l = l;
+		this.maxtaktzeit = maxtaktzeit;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		try {
+			Eintrag[] eintr = l.getStatus();
+			int groesse = 0;
+			for(Eintrag i: eintr){
+				System.out.println(i.getName()+" : "+i.getAnzahl());
+				groesse += i.getAnzahl();
+			}
+			System.out.println("Insgesamt Produkte: "+groesse);
+			Thread.sleep((int)(Math.random()*this.maxtaktzeit)+1);
+		} catch (InterruptedException e) {
+			System.err.println("Fehler beim Status");
+		}
 		
 	}
 
