1a9b954db01157fd81e5e00eb1793d4da3f86783
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-21 09:26:52 +0200

    Attribute und Methoden hinzugefuegt

--- ErzeugerVerbraucher/Erzeuger.java
+++ ErzeugerVerbraucher/Erzeuger.java
@@ -1,9 +1,22 @@
 package goebel;
 public class Erzeuger implements Runnable{
+	
+	private Lager l;
+	private int taktzeit;
+	private Produkt p;
 
+	public Erzeuger(Lager l, int taktzeit, Produkt p){
+		this.l = l;
+		this.taktzeit = taktzeit;
+	}
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		try {
+			l.lagern(p, 100);
+			Thread.sleep(taktzeit);
+		} catch (InterruptedException e) {
+			System.err.println("Fehler beim Erzeuger.");
+		}
 		
 	}
 
