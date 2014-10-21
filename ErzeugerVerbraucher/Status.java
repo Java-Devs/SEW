f41514c5c64c266f5531d93eeae4855de61afba5
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-21 17:57:34 +0200

    Gibt nur Gesamtprodukte aus

--- ErzeugerVerbraucher/Status.java
+++ ErzeugerVerbraucher/Status.java
@@ -10,17 +10,20 @@ public class Status implements Runnable{
 
 	@Override
 	public void run() {
+		while(true){
 		try {
 			Eintrag[] eintr = l.getStatus();
 			int groesse = 0;
+			if(eintr.length > 0){
 			for(Eintrag i: eintr){
-				System.out.println(i.getName()+" : "+i.getAnzahl());
 				groesse += i.getAnzahl();
 			}
-			System.out.println("Insgesamt Produkte: "+groesse);
+			System.out.println("Insgesamt Produkte: "+groesse+" von "+l.getGroesse()+"\n");
+			}
 			Thread.sleep((int)(Math.random()*this.maxtaktzeit)+1);
 		} catch (InterruptedException e) {
 			System.err.println("Fehler beim Status");
+		}
 		}
 		
 	}
