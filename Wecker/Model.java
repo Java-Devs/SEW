61f4455dbb4efd6fd4d47acc7af57f552e35653b
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-28 18:37:40 +0100

    Aenderung der JLables zur Verfuegung gestellt

--- Wecker/Model.java
+++ Wecker/Model.java
@@ -1,13 +1,17 @@
 package goebel;
 
 import javax.swing.JFrame;
-
+/**
+ * Stellt das Fenster zur Verfuegung
+ * @author Melanie Goebel
+ * @version 2014-10-28
+ */
 public class Model extends JFrame{
 	public Model(View layoutPanel, String titel){
 		super(titel);
 		this.add(layoutPanel);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
+		this.setResizable(false); // Groesse nicht veraenderbar
 		this.setBounds(0, 0, 600, 600);
 		this.pack();
 		this.setLocationRelativeTo(null);
--- Wecker/View.java
+++ Wecker/View.java
@@ -4,7 +4,11 @@ import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+/**
+ * Zeichnet Labels und Buttons
+ * @author Melanie Goebel
+ * @version 2014-10-28
+ */
 public class View extends JPanel{
 	private JButton deaktivieren;
 	private JButton remove;
@@ -12,10 +16,16 @@ public class View extends JPanel{
 	private JLabel status;
 	private JLabel status1;
 	private JLabel status2;
+	private boolean removeEnabled = false;
 	
+	/**
+	 * Zeichnen der Elemente
+	 * @param c das Control (Listener sind dort)
+	 */
 	public View(Control c){
 		deaktivieren = new JButton("Deactivate");
 		remove = new JButton("Remove");
+		remove.setEnabled(removeEnabled);
 		exit = new JButton("Exit");
 		status = new JLabel("Timerstatus:");
 		status1 = new JLabel("Activate");
@@ -33,7 +43,22 @@ public class View extends JPanel{
 		this.add(exit);
 		
 	}
-
+	/**
+	 * Setzt den Status des Weckers, ob er aktiviert oder deaktiviert ist
+	 * @param den status als Text
+	 */
+	public void setStatus1(String status){
+		status1.setText(status);
+		this.updateUI();
+	}
+	/**
+	 * Setzt den Status des Weckers, ob der Alarm an ist.
+	 * @param den status als Text
+	 */
+	public void setStatus2(String status){
+		status2.setText(status);
+		this.updateUI();
+	}
 	/**
 	 * Getter Methode fuer den Button namens deaktivieren
 	 * @return den Button namens deaktivieren
@@ -56,5 +81,13 @@ public class View extends JPanel{
 	 */
 	public JButton getExit() {
 		return exit;
+	}
+	/**
+	 * Setzt ob der Remove-Button drückbar ist oder nicht
+	 * @param den status als boolean
+	 */
+	public void setRemoveEnabled(boolean status){
+		this.removeEnabled = status;
+		this.updateUI();
 	}
 }
