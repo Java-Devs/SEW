05948a811fb073c51932717f4eb53e8aa9859a68
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-10-28 19:43:32 +0100

    Letzter Commit

--- AnalogUhr/UhrDisplay.java
+++ AnalogUhr/UhrDisplay.java
@@ -53,7 +53,7 @@ public class UhrDisplay extends JPanel{
 		for (int i = 0; i < 60; i++) { // Die Striche fuer die Minuten
 			g.drawLine(c.gradToX(i*360/60,durchmesser,durchmesser/2),c.gradToY(i*360/60,durchmesser,durchmesser/2),c.gradToX(i*360/60,durchmesser,(int)(durchmesser/2.1)),c.gradToY(i*360/60,durchmesser,(int)(durchmesser/2.1)));
 		}
-	    date = c.getDate();
+	    date = c.getDatum();
 	    
 	    //Datum und die Box fuer das Datum zeichnen
 	    g.drawRect(mittelpunkt+mittelpunkt/4, mittelpunkt-mittelpunkt/11, durchmesser/4, durchmesser/10);
@@ -61,7 +61,7 @@ public class UhrDisplay extends JPanel{
 	    g.drawString(date[3]+","+date[2]+"."+date[1], mittelpunkt+(int)(mittelpunkt/3.5), mittelpunkt+mittelpunkt/20);
 		
 	    // Mond zeichnen
-	    Image pic= new ImageIcon("C:/Users/Bleedinghina/repository/SEW/AnalogUhr/data/"+c.getMondStatus()+".png").getImage();
+	    Image pic= new ImageIcon("data/"+c.getMondStatus()+".png").getImage();
 		g.drawImage(pic, mittelpunkt/2, (int)(mittelpunkt*1.2), mittelpunkt/3,mittelpunkt/3, null);
 	   
 		zeit = c.getZeit(); // Speichern von Millisekunden[0], Sekunden[1], Minuten[2], Stunden[3]
