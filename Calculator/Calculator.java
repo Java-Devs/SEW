e8a997d373f514fc4283cc4b9691fcd5251bdd36
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-11-10 11:26:56 +0100

    Alle Methoden gefuellt

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,8 +1,10 @@
 package goebel;
 
+import java.util.ArrayList;
 import java.util.List;
 /**
- * 
+ * Rechnet einen modifier mit jeden Wert der Liste values. In die Liste koennen Werte hinzugefuegt und
+ * entfernt werde sowie den modifier geaendert.
  * @author Melanie Goebel
  * @version 2014-11-10
  */
@@ -15,27 +17,53 @@ public class Calculator {
 	private Calcuable operator;
 
 	public Calculator() {
-
+		values = new ArrayList<Double>();
 	}
-
+	/**
+	 * Fuegt einen Wert zur Liste hinzu.
+	 * @param den Wert in double
+	 */
 	public void addValue(double value) {
-
+		values.add(value);
 	}
-
+    /**
+     * Setzt den modifier auf einen Wert
+     * @param den Wert in double
+     */
 	public void setModifier(double modifier) {
-
+		this.modifier = modifier;
 	}
-
+	/**
+	 * 
+	 * @param operator
+	 */
+	public void setOperator(Calcuable operator){
+		
+	}
+	/**
+	 * Loescht einen bestimmten Wert aus der Liste.
+	 * @param den Wert in double
+	 */
 	public void removeValue(double value) {
-
+		values.remove(value);
 	}
-
+	/**
+	 * Rechnet alle Werte aus der Liste mit den modifier.
+	 * @return die Ergebnisse in einer Liste
+	 */
 	public List<Double> processCalculations() {
 		return null;
 	}
-
+    /**
+     * Gibt die Liste als Text zurueck.
+     * @return der Text
+     */
 	public String toString() {
-		return null;
+		String v = "";
+		for(double value: values){
+			v += value + ","; 
+		}
+		return v;
 	}
 
 }
