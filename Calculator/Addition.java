c6d04c06fdec47e55a256388afe61c72ebd6ef90
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-11-10 11:46:31 +0100

    Implementierung von processCalculations

--- Calculator/Addition.java
+++ Calculator/Addition.java
@@ -2,7 +2,7 @@ package goebel;
 
 import java.util.List;
 /**
- *
+ * Addiert alle Werte der Liste um modifier
  * @author Melanie Goebel
  * @version 2014-11-10
  */
@@ -13,7 +13,11 @@ public class Addition implements Calcuable {
 	 * @see Calcuable#processCalculations()
 	 */
 	public List<Double> processCalculations(List<Double> values, double modifier) {
-		return null;
+		List<Double> temp = values;
+		for(int i = 0; i < temp.size(); i++){
+			temp.set(i, temp.get(i)+modifier);
+		}
+		return temp;
 	}
 
 }
--- Calculator/Division.java
+++ Calculator/Division.java
@@ -2,7 +2,7 @@ package goebel;
 
 import java.util.List;
 /**
- * 
+ * Dividiert alle Werte der Liste um modifier (ausser modifier ist 0)
  * @author Melanie Goebel
  * @version 2014-11-10
  */
@@ -13,7 +13,16 @@ public class Division implements Calcuable {
 	 * @see Calcuable#processCalculations()
 	 */
 	public List<Double> processCalculations(List<Double> values, double modifier) {
-		return null;
+		if(modifier != 0.0){
+		List<Double> temp = values;
+		for(int i = 0; i < temp.size(); i++){
+			temp.set(i, temp.get(i)/modifier);
+		}
+		return temp;
+		}else{
+			System.err.println("Division durch 0 nicht moeglich!!");
+			return null;
+		}
 	}
 
 }
