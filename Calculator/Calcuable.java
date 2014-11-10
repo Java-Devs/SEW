820215282d07115072c20f678efaaaa9830a2d8d
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-11-10 10:58:05 +0100

    Alle java-Files in package goebel und Klassenkommentare hinzugefuegt

--- Calculator/Calcuable.java
+++ Calculator/Calcuable.java
@@ -1,5 +1,11 @@
-import java.util.List;
+package goebel;
 
+import java.util.List;
+/**
+ * 
+ * @author Melanie Goebel
+ * @version 2014-11-10
+ */
 public interface Calcuable {
 
 	public List processCalculations();
--- Calculator/Substraction.java
+++ Calculator/Substraction.java
@@ -1,5 +1,11 @@
-import java.util.List;
+package goebel;
 
+import java.util.List;
+/**
+ * 
+ * @author Melanie Goebel
+ * @version 2014-11-10
+ */
 public class Substraction implements Calcuable {
 
 
